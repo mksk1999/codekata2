2ef1c23cbde10db61485388436127430135732e8
saikumar <51634270+mksk1999@users.noreply.github.com>
2019-07-18 17:33:34 +0530

    Update pl1.py

--- pl1.py
+++ pl1.py
@@ -1,7 +1,11 @@
 s=input()
 b=[]
 for i in s:
-    d=chr(ord(i)+3)
-    b.append(d)
+    if(i>='A' and i<='V'):
+        d=chr(ord(i)+3)
+        b.append(d)
+    else:
+        c=chr(ord(i)-25)
+        b.append(c)   
 for i in b:
     print(i,end="")
