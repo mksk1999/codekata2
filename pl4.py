5badc3665c5ea29ca6bc5c72e7fec6ec4d273b89
saikumar <51634270+mksk1999@users.noreply.github.com>
2019-07-18 17:48:09 +0530

    Update pl4.py

--- pl4.py
+++ pl4.py
@@ -1,6 +1,6 @@
 n,k=map(int,input().split())
-l=list(map(int,input().split()))
+a=list(map(int,input().split()))
 m=list(map(int,input().split()))
 for i in m:
-    l.append(i)
+    a.append(i)
     print(max(l),end=" ")
