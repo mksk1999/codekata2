aa19fc8ae1a386617c5615f2d5106f80c55985f7
saikumar <51634270+mksk1999@users.noreply.github.com>
2019-07-18 21:09:35 +0530

    Create p17.py

---
+++ p17.py
@@ -0,0 +1,12 @@
+s=input()
+a=s.replace(' ','')
+for i in a:
+    if(i>='a' and i<='z'):
+        if a.count(i)==1:
+            print(i,end=" ")
+    else:
+        if a.count(i)==1:
+            print(i,end=" ")
+    
+    
+ 
